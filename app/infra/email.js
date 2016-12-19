c5f7714f053ff2d361339fa3506c9d128ffb7dbb
BuenoOtavio <programador.bueno@gmail.com>
2016-12-19 12:02:43 -0200

    Finalizado o processo substituir template

--- app/infra/email.js
+++ app/infra/email.js
@@ -2,25 +2,34 @@
 
 module.exports = (app) => {
 
-    //what returns  
-    // const infra = {
-    //     configEmail,
-    //     receiver,
-    //     from,
-    //     contentcadas
-    // }
-
-    // return infra
-
-
     const createEmail = email => ({ email })
 
+    const isArray = obj => Object.prototype.toString.call(obj) === '[object Array]'
+
+    const hasLength = (array) => {
+        return isArray(array) && array.length > 0
+    }
+
+    const initArray = array => {
+        if (!isArray(array))
+            array = []
+        return array;
+    }
+    
+    const templateId = (obj) => (template_id) => {
+        obj.template_id = template_id
+        return obj
+    }
+
     const configEmail = (email) => (body) => (token) => {
-        const sg = require('sendgrid')(token)
+        const sg = require('sendgrid')(toke)
         email.body = {
             personalizations: body.personalizations,
             content: body.content,
             from: body.from
+        }
+        if ('template_id' in body) {
+            email.body.template_id = body.template_id
         }
         const request = sg.emptyRequest(email)
 
@@ -28,10 +37,20 @@ module.exports = (app) => {
     }
 
     const receiver = (obj) => (subject) => (emails) => {
-        obj.personalizations = [{
+        let array = initArray(obj.personalizations)
+        if (hasLength(array)) {
+            array.map((item) => {
+                item.to = emails.map(createEmail)
+                item.subject = subject
+            })
+            obj.personalizations = array
+            return obj
+        }
+        array.push({
             to: emails.map(createEmail),
             subject: subject
-        }]
+        })
+        obj.personalizations = array;
         return obj;
     }
 
@@ -48,11 +67,28 @@ module.exports = (app) => {
         return obj;
     }
 
+    const substitutions = (obj) => (content) => {
+        let array = obj.personalizations
+        array = initArray(array)
+        if (hasLength(array)) {
+            array.map((item) => {
+                item.substitutions = content
+            })
+            obj.personalizations = array;
+            return obj;
+        }
+        array.push({ substitutions: content })
+        obj.personalizations= array;
+        return obj
+    }
+
     const infra = {
         configEmail,
         receiver,
         from,
-        content
+        content,
+        substitutions,
+        templateId
     }
 
     return infra
--- app/routes/sendEmail.js
+++ app/routes/sendEmail.js
@@ -13,25 +13,34 @@ module.exports = (app) => {
                 path: '/v3/mail/send',
             }
             let body = {}
-            
-            body = sendGrid.receiver(body)('Send Email Master blaster')(['emailQueRecebe@exemplo.com'])
-            body = sendGrid.from(body)('emailQueEnvia@exemplo.com')
-            body = sendGrid.content(body)('text/plain')('Assunto aqui')
+
+            body = sendGrid.substitutions(body)({
+                ':name': 'name',
+                ':email': 'emal@example',
+                ':telefone': '11 9999-5512',
+                ':mensagem': 'Test email sender'
+            })
+            body = sendGrid.receiver(body)('Envio de email')(['EmailReceiver@example.com'])
+            body = sendGrid.templateId(body)('templateId')
+            body = sendGrid.from(body)('EmailSender@example.com.br')
+            //contentType example text/html 
+            body = sendGrid.content(body)('contentType')('content')
             const request = sendGrid.configEmail(email)(body)(process.env.sgtoken)
 
             request.then(response => {
                 console.log(response.statusCode);
                 console.log(response.body);
                 console.log(response.headers);
+
+                return res({ process: "finished" })
+            }).catch(error => {
+                //error is an instance of SendGridError
+                //The full response is attached to error.response
+                return res({
+                    process: "finished with error",
+                    error: error.response.statusCode
+                })
             })
-                .catch(error => {
-                    //error is an instance of SendGridError
-                    //The full response is attached to error.response
-                    console.log(error.response.statusCode);
-            });  
-
-          return res('{processo:"finalizado"}')      
-
         }
     })
 }
